166254e3d954d5115ea1b0b0c4e8a9df21d65c12
GentleMadman <lucianopilot19@gmail.com>
2024-10-22 13:33:54 -0300

    Agregado entidad Cata

---
+++ Vinoteca/Entities/Cata.cs
@@ -0,0 +1,12 @@
+﻿namespace Vinoteca.Entities
+{
+    public class Cata
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Nombre { get; set; }
+        public List<string> Invitados { get; set; }
+        public List<Wine> Vinos { get; set; }
+    }
+
+}
